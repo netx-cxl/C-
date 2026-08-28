6e00365568ec6c01d657941d6ac7035f299fe979
Netx <747771334@qq.com>
2026-08-28 20:16:33 +0800

    circulation and calculator， scoresGrade

--- C语言个人作业分享/C-/Netx_homework/basicKnowledgePoints.c
+++ C语言个人作业分享/C-/Netx_homework/basicKnowledgePoints.c
@@ -1,8 +1,9 @@
-#include <stdio.h>//头文字<头文件>
-//主函数
-int main(void)//返回整数不带参数
-{
-    print("hello World");//打印
+#include <stdio.h> //头文字<头文件>
 
-    return 0;//返回结束
+// 主函数
+int main(void) // 返回整数不带参数
+{
+    printf("hello World"); // 打印
+
+    return 0; // 返回结束
 }
